605285217904c19fb819f77def4e67e2bbad97d1
Steve Cho <imstevecho@yahoo.com>
2017-09-18 09:00:26 -0400

    renamed test fil

---
+++ src/counter.test.js
@@ -0,0 +1,25 @@
+import {increaseCount, reducer, store} from './counter'
+
+describe('Counter', function() {
+  it('should have a default state', function () {
+    const result = reducer();
+    expect(result.count).toEqual(0);
+  });
+
+
+  it('should increase the count', function () {
+    const action = increaseCount();
+    const result = reducer(undefined, action);
+    expect(result.count).toEqual(1);
+  });
+
+
+  it('should increase the count with a custom count value', function () {
+    const action = increaseCount(5);
+    const result = reducer(undefined, action);
+    expect(result.count).toEqual(5);
+  });
+
+});
+
+
